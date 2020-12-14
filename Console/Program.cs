73c190eb7c9c6021247027b42de751ee5fe7714f
DESKTOP-RR225US\Student <helpdesk-szkola@altkom.pl>
2020-12-14 12:04:59 +0100

    Pobieranie konfiguracji z pliku

---
+++ Console/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using Microsoft.Extensions.Configuration;
+
+namespace Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+            .AddXmlFile("configapp.xml", optional: false, reloadOnChange: true)
+            .AddJsonFile("configapp.json", optional: true, reloadOnChange: true)
+            .AddIniFile("configapp.ini", optional: true, reloadOnChange: true)
+            .AddYamlFile("configapp.yaml", optional: true, reloadOnChange: true)
+            .Build();
+
+            Hello(config["HelloJson"]);
+            Hello(config["HelloIni"]);
+            Hello(config["HelloXml"]);
+            Hello(config["HelloYaml"]);
+        }
+
+        private static void Hello(string @string)
+        {
+            System.Console.WriteLine(
+                Figgle.FiggleFonts.Standard.Render(
+                $"Hello {@string}!")
+            );
+        }
+    }
+}
